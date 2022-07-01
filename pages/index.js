6be5c237cc87e962dc163fe0bf72417cd1fb401f
Ismael Belisario <ismaelbeli.com@gmail.com>
2022-07-01 17:09:17 -0400

    First2 commit

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,9 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to Algorithms Site Web!" />
+        <p className="description">
+          Working...
+        </p>
       </main>
 
       <Footer />
