0b25c72fe57b13acf2ee57a835ecb3cfcc42c3f6
ismaelxyz <ismaelbeli.com@gmail.com>
2022-07-12 16:52:32 -0400

    Second commit

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my Algorithms Site Web!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Algorithms Site Web official web : )
         </p>
       </main>
 
